0011e8bebb95385af2f4d4150c99c3c0118f9c0d
minjoo1412 <50561483+minjoo1412@users.noreply.github.com>
2020-11-18 18:37:58 +0900

    2020_11_18
    unity_chan! animator

---
+++ Assets/player_controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)){
+            animator.Play("RUN00_F");
+        }else if(Input.GetKeyDown(KeyCode.R)){
+            animator.Play("idle");
+        }
+    }
+}
